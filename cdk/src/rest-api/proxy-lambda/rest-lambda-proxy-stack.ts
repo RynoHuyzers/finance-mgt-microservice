abe224da02414be72b153a9e99aec6b797c9b37b
RynoH <ryno.huyzers@symbiotics.co.za>
2021-06-27 14:21:43 +0200

    ugg

--- cdk/src/rest-api/proxy-lambda/rest-lambda-proxy-stack.ts
+++ cdk/src/rest-api/proxy-lambda/rest-lambda-proxy-stack.ts
@@ -30,6 +30,7 @@ export class FinanceManagementProxyStack extends Stack {
         });
 
         // Rest Proxy Function
+        // @ts-ignore
         const restProxyFnPath: string = `${__dirname}/../../../deploy/rest-api/finance-management-rest-lambda.zip`;
         const restProxyLambda: Function = new Function(this, 'FinanceManagement_RestAPI_ProxyLambda', {
             functionName: 'FinanceManagement-RestProxy',
